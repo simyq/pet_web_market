f4d8aefef88acc536bbcbf1f446c1cb0e0ab8954
simyq <badsurg@gmail.com>
2026-03-05 00:53:59 +0300

    new methods and privacy add

--- src/models.py
+++ src/models.py
@@ -7,12 +7,59 @@ from typing import Union
 class Product:
     """Class for making products"""
 
+    __products_dict = dict()  # Storage only for this project, in big online-shops this must be replaced with DB or smth else
+
+
     def __init__(self, name: str, description: str, price: Union[float, int], quantity: int):
         """Initialization method"""
         self.name = name
         self.description = description
-        self.price = price
+        self.__price = price
         self.quantity = quantity
+        Product.__products_dict[name] = self
+
+
+    @property
+    def price(self):
+        return self.__price
+
+
+    @price.setter
+    def price(self, value):
+        if value <= 0:
+            print("Price cannot be less than or equal to 0")
+            pass
+        elif self.__price > value:
+            print("""Current product price is greater than the given price.
+If you are sure you want to reset the price to lower one, enter 'y', enter any other key to pass""")
+            try:
+                user_input = input().lower().strip()
+                if user_input == "y":
+                    self.__price = value
+            except Exception:
+                pass
+        else:
+            self.__price = value
+
+
+    @classmethod
+    def new_product(cls, product_dict: dict):
+        """Class method for creating a new product object from dictionary"""
+
+        name = product_dict.get('name')
+        description = product_dict.get('description')
+        price = product_dict.get('price')
+        quantity = product_dict.get('quantity')
+
+        if name in cls.__products_dict:
+            existing_product = cls.__products_dict[name]
+            existing_product.quantity += quantity
+            max_price = max(price, existing_product.price)
+            existing_product.price = max_price
+            return existing_product
+
+        else:
+            return cls(name, description, price, quantity)
 
 
 class Category:
@@ -21,14 +68,34 @@ class Category:
     category_count = 0
     product_count = 0
 
+
     def __init__(self, name: str, description: str, products: list[Product]):
         """Initialization method"""
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
+        # self.__products_dict = {p.name: p for p in products} # Storage only for this project, in big online-shops this string must be replaced with DB or smth else
 
         Category.category_count += 1
         Category.product_count += len(products)
+
+
+    def add_product(self, product: Product):
+        """Add product to category"""
+
+        self.__products.append(product)
+        Category.product_count += 1
+
+
+    @property
+    def products(self) -> str:
+        """Getter for list of products in category"""
+
+        products_string = ""
+        for product in self.__products:
+            products_string += f"{product.name}, {product.price} RUB, Stock: {product.quantity}.\n"
+
+        return products_string.strip()
 
 
 def init_from_json(json_file: str) -> tuple[list[Product], list[Category]]:
@@ -62,3 +129,19 @@ def init_from_json(json_file: str) -> tuple[list[Product], list[Category]]:
         all_categories.append(category)
 
     return all_products, all_categories
+
+
+
+
+# prod1 = Product("prod1", "descr1", 1, 100)
+# prod2 = Product("prod2", "descr2", 2, 200)
+#
+# cat = Category("cat", "descr", [prod1])
+#
+# print(cat)
+# print(cat.name)
+#
+# cat.add_product(prod2)
+#
+# print(cat)
+# print(cat.products)
